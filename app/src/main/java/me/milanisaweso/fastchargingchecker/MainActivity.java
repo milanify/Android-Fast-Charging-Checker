d1b30d93fc5dbe0b957cfb218cf657c72cb88cba
Milan <hi.milan102@gmail.com>
2018-08-03 20:12:10 -0700

    Initial commit

---
+++ app/src/main/java/me/milanisaweso/fastchargingchecker/MainActivity.java
@@ -0,0 +1,31 @@
+package me.milanisaweso.fastchargingchecker;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = this.registerReceiver(null, ifilter);
+
+        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+
+        TextView textView = findViewById(R.id.main_textbox);
+        if(isCharging) {
+            textView.setText("We are charging!");
+        }
+
+
+    }
+}
