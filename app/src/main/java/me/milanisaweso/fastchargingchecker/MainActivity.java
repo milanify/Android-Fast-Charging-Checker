42e0763a9a180fff9b7735c231d9f728ff5912bb
Milan <hi.milan102@gmail.com>
2018-08-04 01:21:50 -0700

    Target minimum API version 26

--- app/src/main/java/me/milanisaweso/fastchargingchecker/MainActivity.java
+++ app/src/main/java/me/milanisaweso/fastchargingchecker/MainActivity.java
@@ -1,11 +1,8 @@
 package me.milanisaweso.fastchargingchecker;
 
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,19 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        Intent batteryStatus = this.registerReceiver(null, ifilter);
-
-        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                status == BatteryManager.BATTERY_STATUS_FULL;
-
-        TextView textView = findViewById(R.id.main_textbox);
-        if(isCharging) {
-            textView.setText("We are charging!");
-        }
-
 
     }
 }
--- app/src/main/java/me/milanisaweso/fastchargingchecker/PowerConnectionReceiver.java
+++ app/src/main/java/me/milanisaweso/fastchargingchecker/PowerConnectionReceiver.java
@@ -1,19 +1,34 @@
 package me.milanisaweso.fastchargingchecker;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.BatteryManager;
+import android.service.notification.StatusBarNotification;
+import android.widget.Toast;
 
 public class PowerConnectionReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                status == BatteryManager.BATTERY_STATUS_FULL;
+        boolean acCharge = isACCharge(context);
 
-        int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
-        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+        if(acCharge) {
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            StatusBarNotification[] notificationsArray = notificationManager.getActiveNotifications();
+
+            Toast.makeText(context, "Plugged in via AC: "  + intent.getAction(), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "Plugged in "  + intent.getAction(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static boolean isACCharge(Context context) {
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = context.registerReceiver(null, intentFilter);
+
+        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        return chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
     }
 }
